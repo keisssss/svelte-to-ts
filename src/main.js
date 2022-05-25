3296e433351443c0d905a859eaaa3cc64a4d47ba
keisuke <keisuke.machida@plaid.co.jp>
2022-05-25 12:55:19 +0900

    change child

--- src/main.js
+++ src/main.js
@@ -6,25 +6,29 @@ export const state = writable("state4");
 
 const stateToTarget = {
   state1: {
-    type: "embed",
+    style: "embed",
+    type: "change",
     target: document.querySelector(
       "body > div:nth-of-type(1) > div:nth-of-type(1)"
     ),
   },
   state2: {
-    type: "embed",
+    style: "embed",
+    type: "change",
     target: document.querySelector(
       "body > div:nth-of-type(1) > div:nth-of-type(2)"
     ),
   },
   state3: {
-    type: "embed",
+    style: "embed",
+    type: "change",
     target: document.querySelector(
       "body > div:nth-of-type(1) > div:nth-of-type(3)"
     ),
   },
   state4: {
-    type: "modal",
+    style: "modal",
+    type: "",
     target: document.body,
   },
 };
@@ -39,8 +43,15 @@ function mountReplace(Component, options) {
   frag.style.width = "100%";
   frag.style.top = 0;
   frag.style.left = 0;
-  if (options.target.type === "modal") {
+  if (options.target.style === "modal") {
     frag.style.position = "absolute";
+  } else if (options.target.style === "embed") {
+    if (options.target.type === "change") {
+      const elements = options.target.target.children;
+      while (elements.length) {
+        elements.item(0).remove();
+      }
+    }
   }
 
   //   const frag = document.createDocumentFragment();
