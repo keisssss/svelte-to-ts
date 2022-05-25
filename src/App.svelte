d09d8134a3cd90ec39662b8ee40f9428a5b52169
keisuke <keisuke.machida@plaid.co.jp>
2022-05-25 12:14:57 +0900

    embed to state

---
+++ src/App.svelte
@@ -0,0 +1,101 @@
+<script>
+  export let state;
+  import { beforeUpdate, onMount, afterUpdate, tick } from "svelte";
+  import { writable, get } from "svelte/store";
+  const onClick = (to) => {
+    state.set(to);
+  };
+</script>
+
+{#if $state === "state1"}
+  <div id="svelte-component" class="block" data-state="state1">
+    embed-state1
+    <button
+      on:click={() => {
+        onClick("state2");
+      }}
+    >
+      to state2</button
+    >
+  </div>
+{:else if $state === "state2"}
+  <div id="svelte-component" class="block" data-state="state2">
+    embed-state2
+    <button
+      on:click={() => {
+        onClick("state3");
+      }}
+    >
+      to state3</button
+    >
+  </div>
+{:else if $state === "state3"}
+  <div id="svelte-component" class="block" data-state="state3">
+    embed-state3
+    <button
+      on:click={() => {
+        onClick("state4");
+      }}
+    >
+      to state4</button
+    >
+  </div>
+{:else if $state === "state4"}
+  <div id="svelte-component" class="modal-container" data-state="state3">
+    <div class="modal">
+      modal<button
+        on:click={() => {
+          onClick("state1");
+        }}
+      >
+        to state1</button
+      >
+    </div>
+  </div>
+{:else}
+  <p id="svelte-component">else</p>
+{/if}
+
+<style>
+  .container {
+    /* width: 100%;
+    height: 100%; */
+  }
+  .block {
+    height: 200px;
+    width: 200px;
+    background-color: honeydew;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .modal-container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .modal {
+    position: relative;
+    height: 300px;
+    width: 500px;
+    background-color: honeydew;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    gap: 20px;
+  }
+
+  div {
+    /* height: 100%;
+    width: 100%; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,49 @@
+import App from "./App.svelte";
+import { writable, get } from "svelte/store";
+import { afterUpdate } from "svelte";
+
+export const state = writable("state4");
+
+const stateToTarget = {
+  state1: {
+    type: "embed",
+    target: document.querySelector("body > div:nth-of-type(1)"),
+  },
+  state2: {
+    type: "embed",
+    target: document.querySelector("body > div:nth-of-type(2)"),
+  },
+  state3: {
+    type: "embed",
+    target: document.querySelector("body > div:nth-of-type(3)"),
+  },
+  state4: {
+    type: "modal",
+    target: document.body,
+  },
+};
+
+function mountReplace(Component, options) {
+  document.querySelectorAll("#svelte-container").forEach((e) => {
+    e.remove();
+  });
+  const frag = document.createElement("div");
+  frag.id = "svelte-container";
+  frag.style.height = "100%";
+  frag.style.width = "100%";
+  if (options.target.type === "modal") {
+    frag.style.position = "absolute";
+  }
+
+  //   const frag = document.createDocumentFragment();
+  new Component({ ...options, target: frag });
+  options.target.target.appendChild(frag);
+}
+
+state.subscribe((v) => {
+  console.log("subscribe");
+  mountReplace(App, {
+    target: stateToTarget[v],
+    props: { state },
+  });
+});
